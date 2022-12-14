cf594b9f53cfbb916abb26512b4f9d3b25927053
AdrianJS88 <adriansj1988@yahoo.com>
2022-12-14 17:13:13 +0200

    modificare adaugare animale

---
+++ src/Pig.java
@@ -0,0 +1,12 @@
+public class Pig extends Animal {
+    @Override
+        public void sound() {
+
+            System.out.println("Pig sound");
+            SoundOfAnimals.soundOfAnimal("Pig-Heavy-Screaming-A-www.fesliyanstudios.com.mp3");
+
+    }  @Override
+    public void move() {
+        System.out.println("asta e porc,  si se misca diferit de caine si pisica ");
+    }
+}
